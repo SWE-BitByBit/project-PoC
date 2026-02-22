8bf3a479df12814eb03d2c57604f0743f9718a5d
Riccardo Manisi <riccardomanisi@gmail.com>
2026-02-22 10:48:05 +0100

    Task/frontend uc 6,uc 7,uc 8 (#20)
    * Aggiunta cartelle per parte del Model e UI (inserita solo una cartella).
    - Aggiunte classi per il service e il repository
    (Seguendo linee guida per l'architettura presenti sulla documentazione flutter)
    
    * Modificati path per import
    
    ---------
    
    Co-authored-by: Rmanisi <riccardomansi@gmail.com>

---
+++ frontend/lib/data/services/trusted_contact_api.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../../data/model/trusted_contact_model.dart';
+
+class TrustedContactApi {
+
+  final String baseUrl;
+
+  TrustedContactApi(this.baseUrl);
+
+  Future<List<TrustedContactModel>> fetchContacts() async {
+    final response = await http.get(
+        Uri.parse('$baseUrl/contacts'),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      );
+    
+    if (response.statusCode == 200) {
+      final List<dynamic> decoded = jsonDecode(response.body);
+
+      final List<TrustedContactModel> contacts = [];
+
+      for (var item in decoded) {
+        final contact = TrustedContactModel.fromJson(item);
+        contacts.add(contact);
+      }
+
+      return contacts;
+    } else {
+      throw Exception('Failed to load album');
+    }
+  }
+
+
+  Future<TrustedContactModel> createContact(String name, String email) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/contacts'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(<String,String>{
+        'name': name,
+        'email': email,
+      }),
+    );
+
+    if (response.statusCode != 201) {
+      throw Exception('Failed to create contact');
+    }
+
+    return TrustedContactModel.fromJson(jsonDecode(response.body));
+  }
+
+  Future<void> deleteContact(String id) async {
+    final response = await http.delete(
+      Uri.parse('$baseUrl/contacts/$id')
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete contact');
+    }
+  }
+}
