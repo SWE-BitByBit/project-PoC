c4c99c8b97b24476f486e4f60b52577fbbe08b05
Rmanisi <riccardomansi@gmail.com>
2026-02-23 16:21:27 +0100

    Commit per eseguire checkout

--- backend/diary_notes/AwsDynamoS3Api.py
+++ backend/diary_notes/AwsDynamoS3Api.py
@@ -1,29 +1,10 @@
-import os
-from boto3.dynamodb.conditions import Key
-from botocore.exceptions import ClientError
 import json
-from datetime import datetime, timezone
 from uuid import uuid4
+from boto3.dynamodb.conditions import Key
 
-class AwsDynamoS3Api:
-
-    def __init__(self, dynamoTable, s3Client):
-        self.db_table = dynamoTable
-        self.s3_client = s3Client
-
-    def generate_presigned_url(self, client_method, params, expires_in = 3600):
-        """
-            Create pre-signed action link for uploading file (image in this case) to AWS Bucket.
-        """
-        try:
-            return self.s3_client.generate_presigned_url(
-                ClientMethod=client_method,
-                Params=params,
-                ExpiresIn=expires_in
-            )
-        except ClientError:
-            print("Couldn't get a presigned URL for client method GET /notes.")
-            raise
+class AwsDynamoApi:
+    def __init__(self, table):
+        self.table = table
 
     def response(self, status, body):
         return {
@@ -32,73 +13,68 @@ class AwsDynamoS3Api:
             "body": json.dumps(body)
         }
 
-    def add_note(self, event):
+    def add_contact(self, event):
         body = json.loads(event["body"])
-        user_email = event["requestContext"]["authorizer"]["claims"]["email"]
-        message = body.get('message')
-        file_name = body.get('fileName')
-
-        if not message and not file_name:
-            return self.response(
-                400,
-                {"msg": "A note must contain text, an image, or both"}
-            )
-
-        note_id = str(uuid4())
-        item = {
-            "user_email": user_email,
-            "id": note_id,
-            "created_at": datetime.now(timezone.utc).isoformat()
+        contact_id = str(uuid4())
+        contact_name = body["name"]
+        contact_email = body["email"]
+        contact = {
+            "user_id": event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"],
+            "contact_id": contact_id,
+            "name": contact_name,
+            "email": contact_email
         }
-        
-        if message:
-            item["message"] = message
-        upload_url = None
-        
-        if file_name:
-            s3_key = f"{user_email}/{note_id}/{file_name}"
-            item.update({
-                "s3Key": s3_key,
-                "bucketName": os.environ["BUCKET_NAME"],
-            })
-            upload_url = self.generate_presigned_url(
-                "put_object",
-                {
-                    "Bucket": os.environ["BUCKET_NAME"],
-                    "Key": s3_key,
-                },
-                300
-            )
-        self.db_table.put_item(Item=item)
-        response_body = {"note_id": note_id}
-        if upload_url:
-            response_body["presigned_url"] = upload_url
-        return self.response(201, response_body)
 
-    def list_notes(self, event):
-        user_email = event["requestContext"]["authorizer"]["claims"]["email"]
-        result = self.db_table.query(
-            KeyConditionExpression=Key("user_email").eq(user_email)
+        self.table.put_item(Item=contact)
+        return self.response(
+            201,
+            {
+                "id": contact_id,
+                "name": contact_name,
+                "email": contact_email
+            }
         )
 
-        notes = []
+    def get_contact(self, event):
+        contact_id = event["pathParameters"]["contact_id"]
 
-        for item in result.get("Items", []):
-            note = {
-                "id": item["id"],
-                "message": item.get("message", ""),
-                "created_at": item["created_at"]
+        response = self.table.get_item(
+            Key={
+                "user_id": event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"],
+                "contact_id": contact_id
             }
-            if "s3Key" in item:
-                note["presigned_url"] = self.generate_presigned_url(
-                    "get_object",
-                    {
-                        "Bucket": os.environ["BUCKET_NAME"],
-                        "Key": item["s3Key"],
-                    },
-                    300)
-            notes.append(note)
+        )
+        item = response.get("Item")
+        if not item:
+            return self.response(404, {"msg": "Not found"})
 
-        return self.response(200, notes)
+        return self.response(200, item)
 
+    def delete_contact(self, event):
+        contact_id = event["pathParameters"]["contact_id"]
+        try:
+            response = self.table.get_item(
+                Key={
+                    "user_id": event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"],
+                    "contact_id": contact_id
+                }
+            )
+            
+            item = response.get("Item")
+            if not item:
+                raise KeyError("Item not found")
+            
+            self.table.delete_item(
+                Key={"user_id": event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"], "contact_id": contact_id}
+            )
+            return self.response(200, {"msg": "Deleted"})
+        
+        except KeyError:
+            return self.response(404, {"msg": "Not found"})
     
+    def list_contacts(self, event):
+        response = self.table.query(
+            KeyConditionExpression=Key("user_id").eq(event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"])
+        )
+
+        return self.response(200, response["Items"])
